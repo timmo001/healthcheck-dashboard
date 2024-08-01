6ede1cb0fe7a4a1d233f0e4693de6d0645f7198c
Aidan Timson <aidan@timmo.dev>
2024-08-01 21:06:44 +0100

    Create page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default async function Dashboard() {
+  return (
+    <main className="flex min-h-[48.0rem] flex-col items-center justify-center">
+      <section className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+        Dashboard
+      </section>
+    </main>
+  );
+}
